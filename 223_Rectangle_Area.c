0a8b54603f83e8bcc7fc630aae4480bc7f13f8fa
Dmitry.Abdulov <Dmitry.Abdulov@tymphany.com>
2018-10-23 13:29:14 +0800

    223. Rectangle Area   Medium

---
+++ 223_Rectangle_Area.c
@@ -0,0 +1,43 @@
+/*
+* https://leetcode.com/problems/rectangle-area/description/
+* Medium
+*/
+
+int max(int a, int b)
+{
+    if ( a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int min(int a, int b)
+{
+    if ( a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+{
+    int s1 = abs(C-A)*abs(D-B);
+    int s2 = abs(G-E)*abs(H-F);
+    int lbX = max(A, E);
+    int lbY = max(B,F);
+    int trX = min(C, G);
+    int trY = min(D, H);
+    if(C <= E || G <= A || H <= B || D <= F)
+    {
+        return s1+s2;
+    }
+    return s1 + s2 - abs(trX - lbX)*abs(trY-lbY);
+}
