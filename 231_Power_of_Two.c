6a28c2cbc27453711a7325f7df52181c67625137
vlermuis <vlermuis@gmail.com>
2018-10-26 10:41:59 +0800

    - added one more solution for 231 Power of Two(Easy).

--- 231_Power_of_Two.c
+++ 231_Power_of_Two.c
@@ -22,3 +22,14 @@ bool isPowerOfTwo(int n) {
   }
 
 }
+
+bool isPowerOfTwo(int n) {
+    if (n > 0)
+    {
+        return n && !(n & (n - 1));
+    }
+    else
+    {
+        return false;
+    }
+}
