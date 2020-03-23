283438bbc9fbafc0e2cf44722688ff05157f44a5
ashdom <ashdom@qq.com>
2020-03-23 09:38:31 +0200

    number steps reduce to zero

---
+++ 1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.c
@@ -0,0 +1,24 @@
+/*
+1342. Number of Steps to Reduce a Number to Zero (Easy)
+Runtime: 0 ms, faster than 100.00% of C online submissions for Number of Steps to Reduce a Number to Zero.
+Memory Usage: 5.2 MB, less than 100.00% of C online submissions for Number of Steps to Reduce a Number to Zero.
+*/
+
+int numberOfSteps (int num){
+    int idx = 0;
+    while (num > 0)
+    {
+        if ( 1 & num)
+        {
+            num--;
+        }
+        else
+        {
+            num = num >> 1;
+        }
+        idx++;
+    }
+    return idx;
+
+}
+
