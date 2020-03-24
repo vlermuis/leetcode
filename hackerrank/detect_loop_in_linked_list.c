9e3655444b7387f6e39dceb784abb43f8edecf76
ashdom <ashdom@qq.com>
2020-03-24 14:11:52 +0200

    detect loop in linked list

---
+++ hackerrank/detect_loop_in_linked_list.c
@@ -0,0 +1,36 @@
+/*
+https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=linked-lists
+
+*/
+
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    Node* fast = head;
+    Node* slow = head;
+    
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
