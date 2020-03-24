17ccd063a8d8a220c47a11b5b86be92e557b6a0e
ashdom <ashdom@qq.com>
2020-03-24 12:54:57 +0200

    tmp

--- hackerrank/insert_node_at_specific_position_in_list.c
+++ hackerrank/insert_node_at_specific_position_in_list.c
@@ -1,6 +1,6 @@
 /*
 https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists
-
+ 
 */
 #include <assert.h>
 #include <limits.h>
