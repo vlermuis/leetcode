9054fbe73a1446105259b98551ff7fbf0e3bd1f2
ashdom <ashdom@qq.com>
2020-04-01 13:51:04 +0300

    sets stl

---
+++ hackerrank/sets_stl.cpp
@@ -0,0 +1,50 @@
+/*
+https://www.hackerrank.com/challenges/cpp-sets/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+sets_stl
+*/
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+set<int> s;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int nquery;
+        cin >> nquery;
+        int nnum;
+        cin >> nnum;
+        switch (nquery) 
+        {
+            case 1:
+                s.insert(nnum);
+            break;
+            case 2:
+                s.erase(nnum);
+            break;
+            case 3:
+                set<int>::iterator itr = s.find(nnum);
+                if (itr != s.end())
+                {
+                    cout << "Yes" << endl;
+                }                    
+                else {
+                    cout << "No" << endl;
+                }
+            break;
+        }
+    }  
+
+    return 0;
+}
+
+
+
