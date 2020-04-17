f83029a4b181bdb6dba0d45a53b4ec9b1000bcbd
ashdom <ashdom@qq.com>
2020-04-17 06:20:06 +0300

    letter heights

---
+++ hackerrank/letter_heights_PDF_Viewer.cpp
@@ -0,0 +1,92 @@
+/*
+
+https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the designerPdfViewer function below.
+int designerPdfViewer(vector<int> h, string word) {
+    vector<char> alph{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int idx = 0;
+    int maxh = 0;
+    for (auto i = 0; i < word.size(); i++)
+    {
+        auto itr = find(alph.begin(), alph.end(), word.at(i));
+        if (itr != alph.end())
+        {
+            int pos = itr - alph.begin();
+            if (maxh < h.at(pos))
+            {
+                maxh = h.at(pos);
+            }
+        }
+    }
+
+    return (maxh * word.size());
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split_string(h_temp_temp);
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
