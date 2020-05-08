38b58adb778cb371cd1723e2a9e2fed0dfbb6b04
ashdom <ashdom@qq.com>
2020-05-08 07:05:07 +0300

    optimize 1170

--- 1170_Compare_Strings_by_Frequency_of_the_Smallest_Character.cpp
+++ 1170_Compare_Strings_by_Frequency_of_the_Smallest_Character.cpp
@@ -2,8 +2,8 @@
 1170. Compare Strings by Frequency of the Smallest Character (Easy)
 
 https://leetcode.com/problems/compare-strings-by-frequency-of-the-smallest-character/
-Runtime: 32 ms, faster than 59.20% of C++ online submissions for Compare Strings by Frequency of the Smallest Character.
-Memory Usage: 11.9 MB, less than 100.00% of C++ online submissions for Compare Strings by Frequency of the Smallest Character.
+Runtime: 24 ms, faster than 77.99% of C++ online submissions for Compare Strings by Frequency of the Smallest Character.
+Memory Usage: 12 MB, less than 100.00% of C++ online submissions for Compare Strings by Frequency of the Smallest Character.
 */
 
 class Solution {
@@ -25,21 +25,19 @@ public:
     }
     
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<int> result;
-        vector<int> queries_cf;
         vector<int> words_cf;
-        for (auto i = 0; i < queries.size(); i++)
-        {
-            queries_cf.push_back(funcF(queries[i]));
-        }
         for (auto i = 0; i < words.size(); i++)
         {
             words_cf.push_back(funcF(words[i]));
         }
         sort(words_cf.begin(), words_cf.end());
-        for (auto i = 0; i < queries_cf.size(); i++)
+        for (auto i = 0; i < queries.size(); i++)
         {
-            auto f = upper_bound(words_cf.begin(), words_cf.end(), queries_cf[i]);
+            auto f = upper_bound(words_cf.begin(), words_cf.end(), funcF(queries[i]));
             result.push_back(words_cf.end() - f);
         }
 
