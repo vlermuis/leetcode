4b788453acd429767aa6f54e0833680d45222289
ashdom <ashdom@qq.com>
2020-05-13 05:01:22 +0300

    fix typo

--- others/func0.c
+++ others/func0.c
@@ -320,7 +320,7 @@ void reverse_string(char* str)
     for (i = 0; i < len/2; i++)
     {
         char temp = str[i];
-        str[i] = str[len - 1 - i];\
+        str[i] = str[len - 1 - i];
         str[len - 1 - i] = temp;
     }
 }
