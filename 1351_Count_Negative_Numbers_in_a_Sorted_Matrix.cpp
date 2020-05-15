180987466b6f287207588f7ce97e38967fee3bfb
ashdom <ashdom@qq.com>
2020-05-15 11:31:01 +0300

    .

--- 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
+++ 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.cpp
@@ -2,13 +2,15 @@
 1351. Count Negative Numbers in a Sorted Matrix (Easy)
 
 https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
-Runtime: 44 ms, faster than 6.73% of C++ online submissions for Count Negative Numbers in a Sorted Matrix.
+Runtime: 28 ms, faster than 30.87% of C++ online submissions for Count Negative Numbers in a Sorted Matrix.
 Memory Usage: 10.6 MB, less than 100.00% of C++ online submissions for Count Negative Numbers in a Sorted Matrix.
 */
 
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);cout.tie(0);        
         int result = 0;
         for (auto i = 0; i < grid.size(); i++)
         {
